483ef99a22632208663c5ae72fd34011d2f3e723
nglsena0722c <nglsena0722@com2us.com>
2024-11-07 17:48:21 +0900

    Fix : fix css bug

--- src/Popup/Item.tsx
+++ src/Popup/Item.tsx
@@ -140,9 +140,7 @@ const Item = ({
         }
 
         <div className={clsx("relative border-[3px] rounded-[16px] w-full h-full overflow-hidden", {
-            'bg-[#F6F9FF] border-[#D8E1E9] drop-shadow-[2px_2px_#D8E1E9]': (showingItem?.tap === "Item") ||
-                (typeof position !== "number") ||
-                (inventoryTap === "Item"),
+            'bg-[#F6F9FF] border-[#D8E1E9] drop-shadow-[2px_2px_#D8E1E9]': showingItem ? (showingItem.tap === "Item") : (typeof position !== "number" || inventoryTap === "Item"),
             'bg-[#F9FFFE] border-[#C8E9E8] drop-shadow-[2px_2px_#C8E9E8]': showingItem ? showingItem.tap === "Item NFT" : (typeof position === "number" && (inventoryTap === 'Item NFT')),
             'bg-[#FFF9F9] border-[#FFDEDE] drop-shadow-[2px_2px_#FFDEDE]': showingItem ? showingItem.tap === "Other NFT" : (typeof position === "number" && (inventoryTap === 'Other NFT')),
         })}>
@@ -155,9 +153,7 @@ const Item = ({
             </div>
             <div className="z-10 absolute top-0 left-0 border-white border-t-4 border-l-4 w-full h-full rounded-[13px]" />
             <div className={clsx("z-20 absolute top-[4px] left-[4px] w-[calc(100%-4px)] h-[calc(100%-4px)]  rounded-tl-[13px] rounded-br-[13px]", {
-                'bg-[#F6F9FF]': (showingItem?.tap === "Item") ||
-                    (typeof position !== "number") ||
-                    (inventoryTap === "Item"),
+                'bg-[#F6F9FF]': showingItem ? (showingItem.tap === "Item") : (typeof position !== "number" || inventoryTap === "Item"),
                 'bg-[#F9FFFE]': showingItem ? showingItem.tap === "Item NFT" : (typeof position === "number" && (inventoryTap === 'Item NFT')),
                 'bg-[#FFF9F9]': showingItem ? showingItem.tap === "Other NFT" : (typeof position === "number" && (inventoryTap === 'Other NFT')),
             })} />
